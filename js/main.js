d452a1d85f0d9e7d3ef13d617067827bd05fca47
watulo paul <watulopaul235@gmail.com>
2023-10-11 16:34:49 +0300

    intial commit

---
+++ js/main.js
@@ -0,0 +1,104 @@
+
+//change navbar background color on scroll
+$(window).scroll(function(){
+    $("nav").toggleClass("scrolled", $(this).scrollTop()>50);
+})
+
+//back to top button
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// Categories gallery and filter  
+let list = document.querySelectorAll('.list');
+let itemBox = document.querySelectorAll('.itemBox');
+
+for(let i = 0; i<list.length; i++){
+    list[i].addEventListener('click', function(){
+        for(let j = 0; j<list.length; j++){
+            list[j].classList.remove('active');
+        }
+        this.classList.add('active');
+
+        let dataFilter = this.getAttribute('data-filter');
+
+        for(let k = 0; k<itemBox.length; k++){
+            itemBox[k].classList.remove('active');
+            itemBox[k].classList.add('hide');
+        
+        if(itemBox[k].getAttribute('data-item') == dataFilter || dataFilter == "all"){
+            itemBox[k].classList.remove('hide');
+            itemBox[k].classList.add('active');
+        }
+    }
+    })
+}
+
+//owl carousel
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  autoplay:true,
+  loop:true,
+  autoplayTimeout:3000,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:4
+      },
+      1400:{
+        items:3
+    }
+  }
+})
+
+//accordions/ faqs
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+ //Testimonials slider 
+ new Swiper('.testimonials-slider', {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+});
